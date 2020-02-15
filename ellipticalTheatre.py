18c98ed4e33b207e612a6bb6f865e9454ac48292
Macbook Pro <williamneveu@gmail.com>
2020-02-14 20:22:23 -0500

    added tolerance in equality

--- ellipticalTheatre.py
+++ ellipticalTheatre.py
@@ -15,6 +15,10 @@ else:
     X_MAX = 10365
     Y_MAX = 7692
 
+TOL = 0.0001
+
+def almost_equal(x,y):
+    return abs(x-y) < TOL
 
 class Sound_vector():
     def __init__(self,coord,angle):
@@ -40,7 +44,7 @@ def collision_line_ellipse(v, e_param):
     cangle = v.angle
     (a,b,h,k) = e_param
 
-    print(x0,y0,cangle, "\n")
+    print(x0,y0,degrees(cangle), "\n")
 
     # dir = 1 if vector looks up, -1 otherwise
     dir = -1
@@ -52,7 +56,7 @@ def collision_line_ellipse(v, e_param):
         d = y0
         x1 = h + dir * a/b * sqrt(b**2 - d**2 - k**2 + 2*d*k)
         x1_alt = h - dir * a/b * sqrt(b**2 - d**2 - k**2 + 2*d*k)
-        if x1 == x0: x1 = x1_alt
+        if almost_equal(x0,x1): x1 = x1_alt
         y1 = y0
 
     #vertical line
@@ -61,7 +65,7 @@ def collision_line_ellipse(v, e_param):
         d = x0
         y1 = k + dir * b/a * sqrt(a**2 - d**2 - h**2 + 2*d*h)
         y1_alt = k - dir * b/a * sqrt(a**2 - d**2 - h**2 + 2*d*h)
-        if y1 == y0: y1 = y1_alt
+        if almost_equal(y0,y1): y1 = y1_alt
         x1 = x0
 
     else:
@@ -79,7 +83,7 @@ def collision_line_ellipse(v, e_param):
         # big bertha to find x1, intersection of line/vector and ellipse
         x1 = (b**2*k - a**2*m*phi + dir * a*b*sqrt(b**2 + (a*m)**2 - 2*m*phi*h - phi**2 - (m*h)*2)) / (b**2 + (a*m)**2)
         x1_alt = (b**2*k - a**2*m*phi - dir * a*b*sqrt(b**2 + (a*m)**2 - 2*m*phi*h - phi**2 - (m*h)*2)) / (b**2 + (a*m)**2)
-        if x1 == x0: x1 = x1_alt
+        if almost_equal(x0,x1): x1 = x1_alt
         y1 = m*x1 + c
 
     # ellipse's tangent angle at x1,y1
@@ -97,13 +101,13 @@ def collision_line_ellipse(v, e_param):
     v.add_coord((x1,y1))
     v.angle = nangle
 
-    if random.random() > 0.8: v.bounce = False
+    if random.random() > 0.9: v.bounce = False
 
 
 
 #ellipse parameters (a,b,h,k)
-e_param = (.5,.5,0,0)
-vects = [Sound_vector((-sqrt(.5)/2, 0), theta) for theta in range(0,360,45)]
+e_param = (sqrt(2),sqrt(2),0,0)
+vects = [Sound_vector((-1, 0), theta) for theta in range(0,360,45)]
 
 
 
